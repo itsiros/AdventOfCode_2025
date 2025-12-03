2c7720f64a5342e31c201435828cb88dd0770caf
Ioannis Tsiros <jtsiros93@gmail.com>
2025-12-03 12:41:24 +0100

    Part2Done

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -14,30 +15,47 @@ const CLICK = 0
 const LEFT = "L"
 const RIGHT = "R"
 
-func findNextPos(line string, pos int) (int, error) {
+func findNextPos(line string, pos int) (int, int, int, error) {
 
 	if len(line) < 2 {
-		return 0, fmt.Errorf("line with wrong format %s", line)
+		return 0, 0, 0, fmt.Errorf("line with wrong format %s", line)
 	}
 
 	if !strings.HasPrefix(line, RIGHT) && !strings.HasPrefix(line, LEFT) {
-		return 0, fmt.Errorf("line doesnt start with left or right %s", line)
+		return 0, 0, 0, fmt.Errorf("line doesnt start with left or right %s", line)
 	}
 
 	num, err := strconv.Atoi(line[1:])
 	if err != nil {
-		return 0, fmt.Errorf("atoi failed %s", line)
+		return 0, 0, 0, fmt.Errorf("atoi failed %s", line)
 	}
 
-	if line[0] == RIGHT[0] {
-		pos += num
-	} else {
-		pos -= num
+	if line[0] != RIGHT[0] {
+		num = -num
 	}
 
-	pos = (pos%RANGE + RANGE) % RANGE
-	fmt.Println("Pos is ", pos)
-	return pos, nil
+	newPos := pos + num
+	crosses := newPos / RANGE
+	if newPos < 0 && newPos%RANGE != 0 {
+		crosses--
+	}
+
+	crosses = int(math.Abs(float64(crosses)))
+
+	if pos == 0 && line[0] != RIGHT[0] {
+		crosses--
+	}
+
+	part1 := 0
+	pos = (newPos%RANGE + RANGE) % RANGE
+	if pos == CLICK {
+		part1++
+		if line[0] != RIGHT[0] {
+			crosses++
+		}
+	}
+
+	return pos, part1, crosses, nil
 }
 
 func main() {
@@ -55,27 +73,28 @@ func main() {
 	defer file.Close()
 
 	currentPos := STARTING_POS
-	count := 0
+	totalPart1 := 0
+	totalPart2 := 0
+	countPart1 := 0
+	countPart2 := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-
-		currentPos, err = findNextPos(scanner.Text(), currentPos)
+		currentPos, countPart1, countPart2, err = findNextPos(scanner.Text(), currentPos)
 
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			return
 		}
 
-		if currentPos == CLICK {
-			count++
-		}
+		totalPart1 += countPart1
+		totalPart2 += countPart2
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "scanner error: ", err)
 	}
 
-	fmt.Println("The password is ", count)
+	fmt.Println("The password for part1 is: ", totalPart1)
+	fmt.Println("The password for part2 is: ", totalPart2)
 }
