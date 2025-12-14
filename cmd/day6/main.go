e0ea1ce3d14f9500fe3556390bd169ecaee167c4
Ioannis Tsiros <itsiros@2-E-8.42heilbronn.de>
2025-12-14 16:42:42 +0100

    Day6 gives me trouble

--- cmd/day6/main.go
+++ cmd/day6/main.go
@@ -8,6 +8,87 @@ import (
 	"strings"
 )
 
+func complexMath(c []string, isAdd bool) int {
+
+	maxLen := 0
+	for i:= range c {
+		numLen := len(c[i])
+		if maxLen < numLen {
+			maxLen = numLen
+		}
+	}
+
+	var table []string
+
+	k := 1
+	for i := maxLen - 1; i >= 0; i-- {
+		var num strings.Builder
+		for j := range c {
+
+			numLen := len(c[j])
+			if (isAdd && i >= numLen) || (!isAdd && numLen - k < 0) {
+				continue
+			}
+
+			if isAdd {
+				num.WriteByte(c[j][i])
+			} else {
+				num.WriteByte(c[j][numLen - k])
+			}
+		}
+		k++
+		table = append(table, num.String())
+	}
+
+	res := 0
+	if !isAdd {
+		res = 1
+	}
+
+	for t := range table {
+		num, err := strconv.Atoi(table[t])
+		if err != nil {
+			panic(err)
+		}
+		if isAdd {
+			res += num
+		} else {
+			res *= num
+		}
+	}
+
+	// if !isAdd {
+	fmt.Print(table)
+		fmt.Println(" Total :",res)
+	// }
+	return res
+}
+
+func doMathPart2(table [][]string) int  {
+
+	if len(table) == 0 {
+		return 0
+	}
+
+	res := 0
+	numRows := len(table)
+	numCols := len(table[0])
+
+	for col := range numCols {
+		var Colums []string
+		isAdd := false
+		for row := 0; row < numRows-1; row++ {
+			if table[numRows-1][col] == "+" {
+				isAdd = true
+			}
+			Colums = append(Colums, table[row][col])
+		}
+		res += complexMath(Colums, isAdd)
+	}
+
+	return res
+}
+
 func doMath(table [][]string) int {
 
 	if len(table) == 0 {
@@ -18,7 +99,7 @@ func doMath(table [][]string) int {
 	numRows := len(table)
 	numCols := len(table[0])
 
-	for col := 0; col < numCols; col++ {
+	for col := range numCols{
 		total := 0
 		isAdd := false
 		for row := 0; row < numRows-1; row++ {
@@ -39,10 +120,8 @@ func doMath(table [][]string) int {
 				total *= num
 			}
 		}
-		fmt.Println(total)
 		res += total
 	}
-
 	return res
 }
 
@@ -66,8 +145,9 @@ func main() {
 		math = append(math, strings.Fields(scanner.Text()))
 	}
 
-	total := 0
-	total = doMath(math)
+	total := doMath(math)
+	num := doMathPart2(math)
 
 	fmt.Println("The solution for part 1 is:", total)
+	fmt.Println("The solution for part 2 is:", num)
 }
